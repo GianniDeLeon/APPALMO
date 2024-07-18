ef0d4aeed82088761e306c4fc187f7617835112e
GianniDeLeon <2486034770301@ingenieria.usac.edu.gt>
2024-07-17 23:27:23 -0600

    ADD: se agrega pantalla de edición de productos

--- app/src/main/java/com/almoapp/Models/Producto.java
+++ app/src/main/java/com/almoapp/Models/Producto.java
@@ -1,6 +1,8 @@
 package com.almoapp.Models;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Producto {
+public class Producto implements Parcelable {
     private String nombre;
     private String descripcion;
     private String monto;
@@ -9,6 +11,36 @@ public class Producto {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.monto = monto;
+    }
+
+    protected Producto(Parcel in) {
+        nombre = in.readString();
+        descripcion = in.readString();
+        monto = in.readString();
+    }
+
+    public static final Creator<Producto> CREATOR = new Creator<Producto>() {
+        @Override
+        public Producto createFromParcel(Parcel in) {
+            return new Producto(in);
+        }
+
+        @Override
+        public Producto[] newArray(int size) {
+            return new Producto[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(nombre);
+        dest.writeString(descripcion);
+        dest.writeString(monto);
     }
 
     public String getNombre() {
