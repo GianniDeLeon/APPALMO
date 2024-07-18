8a1e8da36009fb74fb3406f7e20b543a2d65b89e
GianniDeLeon <2486034770301@ingenieria.usac.edu.gt>
2024-07-17 19:56:34 -0600

    ADD: se agrega modelo y vistas de facturas quemadas

---
+++ app/src/main/java/com/almoapp/ui/home/Adapter/FacturaAdapter.java
@@ -0,0 +1,56 @@
+package com.almoapp.ui.home.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.almoapp.Models.Factura;
+import com.almoapp.R;
+
+import java.util.List;
+
+public class FacturaAdapter extends RecyclerView.Adapter<FacturaAdapter.FacturaViewHolder>{
+
+    private List<Factura> facturaList;
+
+    public FacturaAdapter(List<Factura> facturaList) {
+        this.facturaList = facturaList;
+    }
+    @NonNull
+    @Override
+    public FacturaAdapter.FacturaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_factura, parent, false);
+        return new FacturaViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FacturaAdapter.FacturaViewHolder holder, int position) {
+        Factura factura = facturaList.get(position);
+        holder.textViewNoFactura.setText(factura.getNoFactura());
+        holder.textViewDescripcion.setText(factura.getDescripcion());
+        holder.textViewMonto.setText(factura.getMonto());
+        holder.textViewEstado.setText(factura.getEstado());
+    }
+
+    @Override
+    public int getItemCount() {
+        return facturaList.size();
+    }
+
+    public static class FacturaViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewNoFactura, textViewDescripcion, textViewMonto, textViewEstado;
+
+        public FacturaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewNoFactura = itemView.findViewById(R.id.textViewNoFactura);
+            textViewDescripcion = itemView.findViewById(R.id.textViewDescripcion);
+            textViewMonto = itemView.findViewById(R.id.textViewMonto);
+            textViewEstado = itemView.findViewById(R.id.textView4);
+        }
+    }
+}
--- app/src/main/java/com/almoapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/almoapp/ui/home/HomeFragment.java
@@ -9,12 +9,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.almoapp.databinding.FragmentHomeBinding;
+import com.almoapp.ui.home.Adapter.FacturaAdapter;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private RecyclerView recyclerView;
+    private FacturaAdapter facturaAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +29,12 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        recyclerView = binding.recyclerViewFacturas;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        facturaAdapter = new FacturaAdapter(homeViewModel.getFacturaList().getValue());
+        recyclerView.setAdapter(facturaAdapter);
+
         return root;
     }
 
